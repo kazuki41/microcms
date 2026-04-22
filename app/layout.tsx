bbe8b2c2ebdd54e8e46407410eda2bdeae4f9461
小濱 一樹 <kazuki441141@gmail.com>
2026-04-22 11:56:52 +0900

    メインビジュアル修正

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import Header from '../components/header';
+import '../styles/globals.scss';
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="ja">
+            <body>
+                <Header/>
+
+                <main>
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}

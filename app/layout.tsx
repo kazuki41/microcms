d478a252e8599db7d499af76342d278112ae1b46
Kohama Kazuki <139529316+kazuki41@users.noreply.github.com>
2026-06-19 09:39:08 +0900

    Add metadata for robots in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import "../styles/globals.scss";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false, 
+    follow: false,
+  },
+};
 
 export default function RootLayout({
   children,
